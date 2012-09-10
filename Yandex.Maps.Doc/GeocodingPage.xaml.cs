3b2d65e5fc4095499321bc2b3a828087102ae266
Konstantin Zaikin <kzaikin@yandex-team.ru>
2012-09-10 19:01:45 +0400

    Update RX to 2.0 release version, MapKit to 1.01 version

---
+++ Yandex.Maps.Doc/GeocodingPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Yandex.Maps.Geocoding;
+using Yandex.Maps.Geocoding.Interfaces;
+using Yandex.Positioning;
+using Yandex.WebUtils.Events;
+
+namespace Yandex.Maps.Doc
+{
+    /// <summary><see cref="GeocodeManager"/> control demonstration.
+    /// </summary>
+    public partial class GeocodingPage
+    {
+        private IGeocodeManager _geocodeManager;
+
+        public GeocodingPage()
+        {
+            InitializeComponent();
+
+            Loaded += GeocodingPageLoaded;
+        }
+
+        void GeocodingPageLoaded(object sender, RoutedEventArgs e)
+        {
+            // init geocode manaager instance and subscribe to its events
+            _geocodeManager = Yandex.Maps.Helpers.GeocodeManagerProvider.GetGeocodeManager();
+
+            _geocodeManager.GeocodeCompleted += GeocodeManagerGeocodeCompleted;
+            _geocodeManager.GeocodeFailed += GeocodeManagerOnGeocodeFailed;
+        }
+
+        private void GeocodeManagerOnGeocodeFailed(object sender, RequestFailedEventArgs<GeocodeRequestParameters> requestFailedEventArgs)
+        {
+            // Warning! Event is fired in non-UI context
+            Dispatcher.BeginInvoke(() => 
+                MessageBox.Show(requestFailedEventArgs.Exception.Message));
+        }
+
+        void GeocodeManagerGeocodeCompleted(object sender, RequestCompletedEventArgs<GeocodeRequestParameters, Geocoding.Dto.GeocodeResult> e)
+        {
+            string messageBoxText = e.RequestResults.Addresses[0].Title;
+
+            // Warning! Event is fired in non-UI context
+            Dispatcher.BeginInvoke(() => 
+                MessageBox.Show(messageBoxText));
+        }
+
+        private void MapTap(object sender, GestureEventArgs e)
+        {
+            Point tapPoint = e.GetPosition(map);
+            GeoCoordinate coordinate = map.ViewportPointToCoordinates(new Media.Point(tapPoint.X, tapPoint.Y));
+            _geocodeManager.Query(new GeocodeRequestParameters(coordinate, null));
+        }
+    }
+}
